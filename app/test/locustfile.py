0877784acf09161d4da3766b02a1eccd78d58796
GlebSolovev <lionyoung@mail.ru>
2021-09-26 23:11:30 +0300

    Improve locus load test

--- app/test/locustfile.py
+++ app/test/locustfile.py
@@ -11,21 +11,23 @@ class TestUser(HttpUser):
         response = self.client.post("/new-user", json={"name": "test user", "wish": "i want to load test"})
         self.secret_key = response.json()["secret_key"]
 
-    @task
+    @task(2)
     def check_happy_person(self):
         self.client.get("/happy-person")
-        time.sleep(1)
+        time.sleep(3)
 
-    @task
+    @task(2)
     def check_user_profile(self) -> int:
-        response = self.client.get("/user/" + self.secret_key)
-        time.sleep(1)
-        return response.json()["balance"]
+        self.client.request_name = "check user profile"
+        balance = self.client.get("/user/" + self.secret_key).json()["balance"]
+        time.sleep(2)
+        return balance
 
-    @task(3)
+    @task(1)
     def support_happy_person(self):
         balance = self.check_user_profile()
         if balance < 1:
             return
         coins = random.randint(1, balance)
+        self.client.request_name = "support happy person"
         self.client.post("/user/" + self.secret_key + "/support/" + str(coins))
