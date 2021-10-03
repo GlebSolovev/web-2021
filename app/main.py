211cabf41c5aa44914e41e273d436907a123e682
GlebSolovev <lionyoung@mail.ru>
2021-10-03 13:46:12 +0300

    Stuck with importing graphene

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+# import sys
+# sys.path.append(r"/home/gleb/tabs/web/venv/lib/python3.8/site-packages")
 
-from .routers import end_points
+from fastapi import FastAPI
+from graphene import ObjectType, String, Schema
+from starlette.graphql import GraphQLApp
+
+
+# from .routers import end_points
+
+
+class Query(ObjectType):
+    hello = String(name=String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return "Hello " + name
+
 
 app = FastAPI()
 
-app.include_router(end_points.router)
+app.add_route("/", GraphQLApp(schema=Schema(query=Query)))
+
+# app.include_router(end_points.router)
